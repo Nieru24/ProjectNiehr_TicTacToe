038855e26760704bdc766c0ab3aca79d852b4f46
Nieru24 <125419386+Nieru24@users.noreply.github.com>
2024-05-08 16:49:34 +0800

    First commit

---
+++ Tic Tac Toe/Form1.Designer.vb
@@ -0,0 +1,191 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btn_1 = New System.Windows.Forms.Button()
+        Me.btn_2 = New System.Windows.Forms.Button()
+        Me.btn_3 = New System.Windows.Forms.Button()
+        Me.btn_4 = New System.Windows.Forms.Button()
+        Me.btn_5 = New System.Windows.Forms.Button()
+        Me.btn_6 = New System.Windows.Forms.Button()
+        Me.btn_7 = New System.Windows.Forms.Button()
+        Me.btn_8 = New System.Windows.Forms.Button()
+        Me.btn_9 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label = New System.Windows.Forms.Label()
+        Me.lbl_score = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btn_1
+        '
+        Me.btn_1.Font = New System.Drawing.Font("Roboto", 15.68317!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_1.Location = New System.Drawing.Point(44, 86)
+        Me.btn_1.Name = "btn_1"
+        Me.btn_1.Size = New System.Drawing.Size(90, 90)
+        Me.btn_1.TabIndex = 0
+        Me.btn_1.UseVisualStyleBackColor = True
+        '
+        'btn_2
+        '
+        Me.btn_2.Font = New System.Drawing.Font("Roboto", 15.68317!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_2.Location = New System.Drawing.Point(133, 86)
+        Me.btn_2.Name = "btn_2"
+        Me.btn_2.Size = New System.Drawing.Size(90, 90)
+        Me.btn_2.TabIndex = 0
+        Me.btn_2.UseVisualStyleBackColor = True
+        '
+        'btn_3
+        '
+        Me.btn_3.Font = New System.Drawing.Font("Roboto", 15.68317!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_3.Location = New System.Drawing.Point(222, 86)
+        Me.btn_3.Name = "btn_3"
+        Me.btn_3.Size = New System.Drawing.Size(90, 90)
+        Me.btn_3.TabIndex = 0
+        Me.btn_3.UseVisualStyleBackColor = True
+        '
+        'btn_4
+        '
+        Me.btn_4.Font = New System.Drawing.Font("Roboto", 15.68317!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_4.Location = New System.Drawing.Point(44, 175)
+        Me.btn_4.Name = "btn_4"
+        Me.btn_4.Size = New System.Drawing.Size(90, 90)
+        Me.btn_4.TabIndex = 0
+        Me.btn_4.UseVisualStyleBackColor = True
+        '
+        'btn_5
+        '
+        Me.btn_5.Font = New System.Drawing.Font("Roboto", 15.68317!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_5.Location = New System.Drawing.Point(133, 175)
+        Me.btn_5.Name = "btn_5"
+        Me.btn_5.Size = New System.Drawing.Size(90, 90)
+        Me.btn_5.TabIndex = 0
+        Me.btn_5.UseVisualStyleBackColor = True
+        '
+        'btn_6
+        '
+        Me.btn_6.Font = New System.Drawing.Font("Roboto", 15.68317!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_6.Location = New System.Drawing.Point(222, 175)
+        Me.btn_6.Name = "btn_6"
+        Me.btn_6.Size = New System.Drawing.Size(90, 90)
+        Me.btn_6.TabIndex = 0
+        Me.btn_6.UseVisualStyleBackColor = True
+        '
+        'btn_7
+        '
+        Me.btn_7.Font = New System.Drawing.Font("Roboto", 15.68317!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_7.Location = New System.Drawing.Point(44, 264)
+        Me.btn_7.Name = "btn_7"
+        Me.btn_7.Size = New System.Drawing.Size(90, 90)
+        Me.btn_7.TabIndex = 0
+        Me.btn_7.UseVisualStyleBackColor = True
+        '
+        'btn_8
+        '
+        Me.btn_8.Font = New System.Drawing.Font("Roboto", 15.68317!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_8.Location = New System.Drawing.Point(133, 264)
+        Me.btn_8.Name = "btn_8"
+        Me.btn_8.Size = New System.Drawing.Size(90, 90)
+        Me.btn_8.TabIndex = 0
+        Me.btn_8.UseVisualStyleBackColor = True
+        '
+        'btn_9
+        '
+        Me.btn_9.Font = New System.Drawing.Font("Roboto", 15.68317!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_9.Location = New System.Drawing.Point(222, 264)
+        Me.btn_9.Name = "btn_9"
+        Me.btn_9.Size = New System.Drawing.Size(90, 90)
+        Me.btn_9.TabIndex = 0
+        Me.btn_9.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Enabled = False
+        Me.Button1.Font = New System.Drawing.Font("Roboto", 15.68317!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(13, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(330, 68)
+        Me.Button1.TabIndex = 1
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label
+        '
+        Me.Label.AutoSize = True
+        Me.Label.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25743!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label.Location = New System.Drawing.Point(143, 18)
+        Me.Label.Name = "Label"
+        Me.Label.Size = New System.Drawing.Size(68, 25)
+        Me.Label.TabIndex = 2
+        Me.Label.Text = "Score"
+        '
+        'lbl_score
+        '
+        Me.lbl_score.AutoSize = True
+        Me.lbl_score.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25743!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_score.Location = New System.Drawing.Point(122, 44)
+        Me.lbl_score.Name = "lbl_score"
+        Me.lbl_score.Size = New System.Drawing.Size(114, 25)
+        Me.lbl_score.TabIndex = 2
+        Me.lbl_score.Text = "0: X    O :0"
+        Me.lbl_score.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(354, 378)
+        Me.Controls.Add(Me.lbl_score)
+        Me.Controls.Add(Me.Label)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btn_9)
+        Me.Controls.Add(Me.btn_6)
+        Me.Controls.Add(Me.btn_3)
+        Me.Controls.Add(Me.btn_8)
+        Me.Controls.Add(Me.btn_7)
+        Me.Controls.Add(Me.btn_5)
+        Me.Controls.Add(Me.btn_4)
+        Me.Controls.Add(Me.btn_2)
+        Me.Controls.Add(Me.btn_1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Tic Tac Toe"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn_1 As Button
+    Friend WithEvents btn_2 As Button
+    Friend WithEvents btn_3 As Button
+    Friend WithEvents btn_4 As Button
+    Friend WithEvents btn_5 As Button
+    Friend WithEvents btn_6 As Button
+    Friend WithEvents btn_7 As Button
+    Friend WithEvents btn_8 As Button
+    Friend WithEvents btn_9 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label As Label
+    Friend WithEvents lbl_score As Label
+End Class
---
+++ Tic Tac Toe/Form1.vb
@@ -0,0 +1,151 @@
+﻿Public Class Form1
+
+
+    Dim x_o As Boolean = True
+    Dim flag As Integer = 0
+    Dim array(2, 2) As String
+    Dim Xscore As Integer = 0
+    Dim Oscore As Integer = 0
+
+    Sub UpdateButton(ByVal btn As Button)
+        If btn.Text = "" And x_o Then
+            btn.Text = "X"
+            x_o = False
+            flag = flag + 1
+        ElseIf btn.Text = "" And Not x_o Then
+            btn.Text = "O"
+            x_o = True
+            flag = flag + 1
+        Else
+            MessageBox.Show("Player has already entered!", "Tic Tac toe")
+        End If
+    End Sub
+
+    Sub WinCheck()
+        For i As Integer = 0 To 2
+            If array(i, 0) <> "" And array(i, 0) = array(i, 1) And array(i, 1) = array(i, 2) Then
+                If array(i, 0) = "X" Then
+                    Xscore += 1
+                Else
+                    Oscore += 1
+                End If
+                MessageBox.Show(array(i, 0) & " has won!")
+                GameReset()
+                Exit Sub
+            End If
+        Next
+
+        For i As Integer = 0 To 2
+            If array(0, i) <> "" And array(0, i) = array(1, i) And array(1, i) = array(2, i) Then
+                If array(i, 0) = "X" Then
+                    Xscore += 1
+                Else
+                    Oscore += 1
+                End If
+                MessageBox.Show(array(0, i) & " has won!")
+                GameReset()
+                Exit Sub
+            End If
+        Next
+
+        If array(0, 0) <> "" And array(0, 0) = array(1, 1) And array(1, 1) = array(2, 2) Then
+            If array(0, 0) = "X" Then
+                Xscore += 1
+            Else
+                Oscore += 1
+            End If
+            MessageBox.Show(array(0, 0) & " has won!")
+            GameReset()
+        ElseIf array(0, 2) <> "" And array(0, 2) = array(1, 1) And array(1, 1) = array(2, 0) Then
+            If array(0, 2) = "X" Then
+                Xscore += 1
+            Else
+                Oscore += 1
+            End If
+            MessageBox.Show(array(0, 2) & " has won!")
+            GameReset()
+        End If
+
+        If flag = 9 Then
+            MessageBox.Show("Game is Tied!")
+            GameReset()
+        End If
+    End Sub
+
+    Sub GameReset()
+        x_o = True
+        flag = 0
+        array = New String(2, 2) {}
+
+        btn_1.Text = ""
+        btn_2.Text = ""
+        btn_3.Text = ""
+        btn_4.Text = ""
+        btn_5.Text = ""
+        btn_6.Text = ""
+        btn_7.Text = ""
+        btn_8.Text = ""
+        btn_9.Text = ""
+        lbl_score.Text = Xscore & ": X    O :" & Oscore
+    End Sub
+
+
+    Private Sub btn_1_Click(sender As Object, e As EventArgs) Handles btn_1.Click
+        UpdateButton(btn_1)
+        array(0, 0) = btn_1.Text
+        WinCheck()
+    End Sub
+
+    Private Sub btn_2_Click(sender As Object, e As EventArgs) Handles btn_2.Click
+        UpdateButton(btn_2)
+        array(0, 1) = btn_2.Text
+        WinCheck()
+    End Sub
+
+    Private Sub btn_3_Click(sender As Object, e As EventArgs) Handles btn_3.Click
+        UpdateButton(btn_3)
+        array(0, 2) = btn_3.Text
+        WinCheck()
+    End Sub
+
+    Private Sub btn_4_Click(sender As Object, e As EventArgs) Handles btn_4.Click
+        UpdateButton(btn_4)
+        array(1, 0) = btn_4.Text
+        WinCheck()
+    End Sub
+
+    Private Sub btn_5_Click(sender As Object, e As EventArgs) Handles btn_5.Click
+        UpdateButton(btn_5)
+        array(1, 1) = btn_5.Text
+        WinCheck()
+    End Sub
+
+    Private Sub btn_6_Click(sender As Object, e As EventArgs) Handles btn_6.Click
+        UpdateButton(btn_6)
+        array(1, 2) = btn_6.Text
+        WinCheck()
+    End Sub
+
+    Private Sub btn_7_Click(sender As Object, e As EventArgs) Handles btn_7.Click
+        UpdateButton(btn_7)
+        array(2, 0) = btn_7.Text
+        WinCheck()
+    End Sub
+
+    Private Sub btn_8_Click(sender As Object, e As EventArgs) Handles btn_8.Click
+        UpdateButton(btn_8)
+        array(2, 1) = btn_8.Text
+        WinCheck()
+    End Sub
+
+    Private Sub btn_9_Click(sender As Object, e As EventArgs) Handles btn_9.Click
+        UpdateButton(btn_9)
+        array(2, 2) = btn_9.Text
+        WinCheck()
+    End Sub
+
+    Private Sub test_Click(sender As Object, e As EventArgs)
+        MessageBox.Show(array(0, 0))
+    End Sub
+
+End Class
